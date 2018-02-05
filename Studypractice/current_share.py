16290e01af6ada1cdada1761df17556642b4d39c
Junkiwang <350187552@qq.com>
2018-02-05 11:51:18 +0800

    fix

--- Studypractice/current_share.py
+++ Studypractice/current_share.py
@@ -1,4 +1,5 @@
 L = input('输入用“/”间隔的均流数据:')
+I = float(input('输入测试时额定输出电压：'))
 L = L.split('/')
 L = list(map(float, L))
 print(L)
@@ -11,7 +12,7 @@ for m in L:
     difference_value = abs(m - average_value)
     l.append(difference_value)
 n = l.index(max(l))
-result = (max(l) / average_value) * 100
+result = (max(l) / (3000/I)) * 100
 print('平均值为：%s' % average_value)
 print('极值为：%s' % L[n])
-print('均流不平衡度为：%s%%' % result)
+print('均流不平衡度为：%.2f%%' % result)
