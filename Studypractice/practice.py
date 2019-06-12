8090e08ca06b71708108db63d47b6ee02e622308
Junkiwang <350187552@qq.com>
2019-06-12 17:57:16 +0800

    add

--- Studypractice/practice.py
+++ Studypractice/practice.py
@@ -766,27 +766,28 @@
 # finally:
 #     connection.close()
 
-# 导入MySQL驱动:
-import mysql.connector
+# # 导入MySQL驱动:
+# import mysql.connector
+#
+# # 注意把password设为你的root口令:
+# conn = mysql.connector.connect(user='root', password='password', database='test')
+# cursor = conn.cursor()
+# try:
+#     # 创建user表:
+#     cursor.execute('create table if not exists user (id varchar(20) primary key, name varchar(20))')
+#     # 插入一行记录，注意MySQL的占位符是%s:
+#     cursor.execute('insert into user (id, name) values (%s, %s)', ['3', 'hichael'])
+#     print(cursor.rowcount)
+#     # 提交事务:
+#     conn.commit()
+# except Exception as msg:
+#     print(msg)
+# finally:
+#     # 运行查询:
+#     cursor = conn.cursor()
+#     cursor.execute('select * from user where id = %s', ('3',))
+#     values = cursor.fetchall()
+#     print(values)
+#     cursor.close()
+#     conn.close()
 
-# 注意把password设为你的root口令:
-conn = mysql.connector.connect(user='root', password='password', database='test')
-cursor = conn.cursor()
-try:
-    # 创建user表:
-    cursor.execute('create table if not exists user (id varchar(20) primary key, name varchar(20))')
-    # 插入一行记录，注意MySQL的占位符是%s:
-    cursor.execute('insert into user (id, name) values (%s, %s)', ['3', 'hichael'])
-    print(cursor.rowcount)
-    # 提交事务:
-    conn.commit()
-except Exception as msg:
-    print(msg)
-finally:
-    # 运行查询:
-    cursor = conn.cursor()
-    cursor.execute('select * from user where id = %s', ('3',))
-    values = cursor.fetchall()
-    print(values)
-    cursor.close()
-    conn.close()
