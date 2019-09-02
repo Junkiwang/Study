156508d0c6b38bc8acc858ef5541da91f0ba1c7f
Junkiwang <350187552@qq.com>
2019-09-02 11:55:51 +0800

    add

--- Studypractice/practice.py
+++ Studypractice/practice.py
@@ -791,17 +791,31 @@
 #     cursor.close()
 #     conn.close()
 
+# import struct
+# a = 12
+# b = struct.pack('>H', a)
+# print(b)
+#
+# import binascii
+# c = b'\x01\x06\x00\x01\x00\x01\x19\xca'
+# d = binascii.hexlify(c)
+# print(d, d[8:12])
+# d = int(d[8:12], 16)
+# print(d)
+#
+# import array
+# print(array.array('H', (0 for i in range(100))))
+
+a = 3.45678
+b = float('%.2f' % a)
+c = round(a, 2)
+
 import struct
-a = 12
-b = struct.pack('>H', a)
-print(b)
-
-import binascii
-c = b'\x01\x06\x00\x01\x00\x01\x19\xca'
-d = binascii.hexlify(c)
-print(d, d[8:12])
-d = int(d[8:12], 16)
-print(d)
-
-import array
-print(array.array('H', (0 for i in range(100))))
+e = b'\x01\x06\x75\x75\x00\x7D\x19\xca'
+f = struct.unpack('>H', e[2:4])[0]  # 大端模式，不加方向则默认小端模式
+print(int(e[2] << 8 & 0xFF00 | e[3]))
+print(bin(e[2] << 8 & 0xFF00 | e[3]))
+print(bin((e[2] + 0xFF00) << 8 | e[3]))
+g = e[4] << 8 | e[5]
+h = struct.unpack('>H', e[4:6])[0]
+print(f, g, h)
